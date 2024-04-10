d5dc6e71d4cde799715308e3d7c0255f7aa8ea66
OleksandrPaslavkyi <156080476+OleksandrPaslavkyi@users.noreply.github.com>
2024-04-10 10:20:27 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/github-deploy', methods=['POST'])
+def github_deploy():
+    return 'POST request to /github-deploy received', 200
+
+if name == '__main__':
+    app.run(debug=True, port=5002)
